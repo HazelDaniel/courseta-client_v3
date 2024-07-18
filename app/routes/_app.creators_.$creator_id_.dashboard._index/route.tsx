bb900cf332adb3311001a717233e43ad2617a4d4
HazelDaniel <hazeldaniel856@gmail.com>
2024-07-18 21:37:44 +0100

    added a modal context to the route

--- app/routes/_app.creators_.$creator_id_.dashboard._index/route.tsx
+++ app/routes/_app.creators_.$creator_id_.dashboard._index/route.tsx
@@ -1,5 +1,8 @@
 import { useOutletContext } from "@remix-run/react";
+import { useMemo, useReducer } from "react";
 import { DashboardBody } from "~/components/dashboard-body";
+import { ModalProvider } from "~/contexts/modal.context";
+import { InitialModalState, ModalReducer } from "~/reducers/modal.reducer";
 
 // import "~/styles/profile.module.css";
 import "~/styles/profile.css";
@@ -7,9 +10,20 @@ import { CreatorProfileType } from "~/types";
 
 export const DashboardIndex: React.FC = () => {
   const { profile } = useOutletContext() as { profile: CreatorProfileType };
-  console.log("hit here");
+  const [modalState, modalDispatch] = useReducer(
+    ModalReducer,
+    InitialModalState
+  );
 
-  return <DashboardBody profile={profile} />;
+  const modalContextValue = useMemo(
+    () => ({ modalState, modalDispatch }),
+    [modalState, modalDispatch]
+  );
+  return (
+    <ModalProvider value={modalContextValue}>
+      <DashboardBody profile={profile} />;
+    </ModalProvider>
+  );
 };
 
 export default DashboardIndex;
