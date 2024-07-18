88491bde62f49d7c8b2ea9154a7c16c9db12eee6
HazelDaniel <hazeldaniel856@gmail.com>
2024-07-18 21:34:15 +0100

    added the route to create new exam

---
+++ app/routes/_app.creators_.$creator_id_.dashboard_.courses_.$course_id_.exams_.new/route.tsx
@@ -0,0 +1,7 @@
+import { AssessmentAdditionSection } from "~/components/assessment-addition-section";
+
+export const ExamAdditionPage: React.FC = () => {
+  return <AssessmentAdditionSection variant="exam" />;
+};
+
+export default ExamAdditionPage;
