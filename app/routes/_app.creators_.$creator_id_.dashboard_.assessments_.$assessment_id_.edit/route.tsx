0f1a8b5abee4b02bc4c302c501fea6518c53d6c7
HazelDaniel <hazeldaniel856@gmail.com>
2024-07-19 13:59:09 +0100

    updated the assessment update route , added state to the accordions (and a context too for modal), split components further

--- app/routes/_app.creators_.$creator_id_.dashboard_.assessments_.$assessment_id_.edit/route.tsx
+++ app/routes/_app.creators_.$creator_id_.dashboard_.assessments_.$assessment_id_.edit/route.tsx
@@ -1,13 +1,26 @@
-import React, { useContext, useMemo, useReducer } from "react";
+import { LoaderFunction, json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import React, { useContext, useMemo, useReducer, useState } from "react";
 import { QuestionAdditionModal } from "~/components/question-addition-modal";
-import { ModalContext, ModalContextValueType, ModalProvider } from "~/contexts/modal.context";
-import { InitialModalState, ModalReducer, __showModal } from "~/reducers/modal.reducer";
+import {
+  ModalContext,
+  ModalContextValueType,
+  ModalProvider,
+} from "~/contexts/modal.context";
+import { questionData } from "~/data/accessment";
+import {
+  InitialModalState,
+  ModalReducer,
+  __showModal,
+} from "~/reducers/modal.reducer";
 import "~/styles/assessment-update.css";
+import { QuestionType } from "~/types";
 
+export const loader: LoaderFunction = () => {
+  return json({ questions: questionData });
+};
 const AddQuestionButton: React.FC = () => {
-  const { modalDispatch } = useContext(
-    ModalContext
-  ) as ModalContextValueType;
+  const { modalDispatch } = useContext(ModalContext) as ModalContextValueType;
   return (
     <button onClick={() => modalDispatch(__showModal("questionAdditionModal"))}>
       <span>
@@ -20,145 +33,71 @@ const AddQuestionButton: React.FC = () => {
   );
 };
 
+const AnswerStatusIcon: React.FC<{ correct?: boolean }> = ({ correct }) => {
+  if (!correct)
+    return (
+      <svg>
+        <use xlinkHref="#cancel"></use>
+      </svg>
+    );
+  return (
+    <svg>
+      <use xlinkHref="#check"></use>
+    </svg>
+  );
+};
+
+export const QuestionUpdateEntry: React.FC<{ question: QuestionType }> = ({
+  question,
+}) => {
+  const [isOpened, setOpened] = useState<boolean>(false);
+  return (
+    <li className={`assessment_table_entry${isOpened ? " current" : ""}`}>
+      <div className="entry_main">
+        <div className="entry_left">
+          <p className="date_text">{question.question}</p>
+          <p className="points_text">{question.points}</p>
+        </div>
+        <div className="entry_right">
+          <span>
+            <svg>
+              <use xlinkHref="#trash"></use>
+            </svg>
+          </span>
+
+          <span onClick={() => setOpened((prev) => !prev)}>
+            <svg>
+              <use xlinkHref="#caret-up"></use>
+            </svg>
+          </span>
+        </div>
+      </div>
+      <div className="entry_detail">
+        <p>Answers</p>
+        {question.options.map((option) => {
+          return (
+            <div className="answer_detail">
+              <span>
+                <AnswerStatusIcon correct={option.correct} key={option.id}/>
+              </span>
+              {option.text}
+            </div>
+          );
+        })}
+      </div>
+    </li>
+  );
+};
+
 export const AssessmentEditTable: React.FC = React.memo(() => {
+  const loadedResult = useLoaderData<typeof loader>() as {
+    questions: QuestionType[];
+  };
   return (
     <ul className="assessment_table_body">
-      <li className={`assessment_table_entry current`}>
-        <div className="entry_main">
-          <div className="entry_left">
-            <p className="date_text">
-              three of the following questions are wrong. your job would be to
-              answer at least two correctly. the first question is:
-            </p>
-            <p className="points_text">200</p>
-          </div>
-          <div className="entry_right">
-            <span>
-              <svg>
-                <use xlinkHref="#trash"></use>
-              </svg>
-            </span>
-
-            <span>
-              <svg>
-                <use xlinkHref="#caret-up"></use>
-              </svg>
-            </span>
-          </div>
-        </div>
-        <div className="entry_detail">
-          <p>Answers</p>
-          <div className="answer_detail">
-            <span>
-              <svg>
-                <use xlinkHref="#cancel"></use>
-              </svg>
-            </span>
-            the application is different and the execution is direct. there is
-            no feasible way to determine if this was going to be
-          </div>
-          <div className="answer_detail">
-            <span>
-              <svg>
-                <use xlinkHref="#check"></use>
-              </svg>
-            </span>
-            the application is different and the execution is direct. there is
-            infact a feasible way
-          </div>
-        </div>
-      </li>
-
-      <li className={`assessment_table_entry`}>
-        <div className="entry_main">
-          <div className="entry_left">
-            <p className="date_text">
-              three of the following questions are wrong. your job would be to
-              answer at least two correctly. the first question is:
-            </p>
-            <p className="points_text">200</p>
-          </div>
-          <div className="entry_right">
-            <span>
-              <svg>
-                <use xlinkHref="#trash"></use>
-              </svg>
-            </span>
-
-            <span>
-              <svg>
-                <use xlinkHref="#caret-up"></use>
-              </svg>
-            </span>
-          </div>
-        </div>
-        <div className="entry_detail">
-          <p>Answers</p>
-          <div className="answer_detail">
-            <span>
-              <svg>
-                <use xlinkHref="#cancel"></use>
-              </svg>
-            </span>
-            the application is different and the execution is direct. there is
-            no feasible way to determine if this was going to be
-          </div>
-          <div className="answer_detail">
-            <span>
-              <svg>
-                <use xlinkHref="#check"></use>
-              </svg>
-            </span>
-            the application is different and the execution is direct. there is
-            infact a feasible way
-          </div>
-        </div>
-      </li>
-      <li className={`assessment_table_entry`}>
-        <div className="entry_main">
-          <div className="entry_left">
-            <p className="date_text">
-              three of the following questions are wrong. your job would be to
-              answer at least two correctly. the first question is:
-            </p>
-            <p className="points_text">200</p>
-          </div>
-          <div className="entry_right">
-            <span>
-              <svg>
-                <use xlinkHref="#trash"></use>
-              </svg>
-            </span>
-
-            <span>
-              <svg>
-                <use xlinkHref="#caret-up"></use>
-              </svg>
-            </span>
-          </div>
-        </div>
-        <div className="entry_detail">
-          <p>Answers</p>
-          <div className="answer_detail">
-            <span>
-              <svg>
-                <use xlinkHref="#cancel"></use>
-              </svg>
-            </span>
-            the application is different and the execution is direct. there is
-            no feasible way to determine if this was going to be
-          </div>
-          <div className="answer_detail">
-            <span>
-              <svg>
-                <use xlinkHref="#check"></use>
-              </svg>
-            </span>
-            the application is different and the execution is direct. there is
-            infact a feasible way
-          </div>
-        </div>
-      </li>
+      {loadedResult.questions.map((question) => {
+      return <QuestionUpdateEntry question={question} key={question.id} />
+      })}
     </ul>
   );
 });
