502af87494746bdbe6a7b8c46b9350852bffb781
HazelDaniel <hazeldaniel856@gmail.com>
2024-07-21 18:27:06 +0100

    added hooks to update a lesson child value and debounce the updates

---
+++ app/hooks/use-update-content-value.ts
@@ -0,0 +1,61 @@
+import { useContext, useEffect, useState } from "react";
+import { useDebounce } from "~/hooks/use-debounce";
+import {
+  LessonUpdateContext,
+  LessonUpdateContextValueType,
+} from "./lesson-update.context";
+import {
+  LessonUpdateActionType,
+  __updateContent,
+  __updateQuiz,
+} from "~/reducers/lesson-update.reducer";
+import { LessonContentFormType, QuizFormType } from "~/types";
+
+export function useUpdateContentValue<T>(defaultValue: T, timeout: number) {
+  const [contentState, setContentState] = useState<T>(defaultValue);
+  const debouncedValue = useDebounce<T>(contentState, timeout);
+  const { lessonUpdateDispatch } = useContext(
+    LessonUpdateContext
+  ) as LessonUpdateContextValueType;
+
+  useEffect(() => {
+    if (debouncedValue) lessonUpdateDispatch(__updateContent(debouncedValue));
+  }, [debouncedValue, lessonUpdateDispatch]);
+  return { setContentState };
+}
+
+export function useUpdateQuizValue<T>(defaultValue: T, timeout: number) {
+  const [quizState, setQuizState] = useState<T>(defaultValue);
+  const debouncedValue = useDebounce<T>(quizState, timeout);
+  const { lessonUpdateDispatch } = useContext(
+    LessonUpdateContext
+  ) as LessonUpdateContextValueType;
+
+  useEffect(() => {
+    if (debouncedValue) lessonUpdateDispatch(__updateContent(debouncedValue));
+  }, [debouncedValue, lessonUpdateDispatch]);
+  return { setQuizState };
+}
+
+export function useUpdateLessonItemValue<
+  T extends Partial<QuizFormType | LessonContentFormType>
+>(defaultValue: T, itemType: "quiz" | "content", timeout: number = 1000) {
+  const [state, dispatch] = useState<T>(defaultValue);
+  const debouncedValue = useDebounce<T>(state, timeout);
+  const updateContext = useContext(LessonUpdateContext);
+  if (!updateContext) {
+    throw new Error("no context at this point");
+  }
+  const { lessonUpdateDispatch } = updateContext;
+
+  useEffect(() => {
+    let actionProducer: LessonUpdateActionType =
+      itemType === "quiz"
+        ? __updateQuiz(debouncedValue)
+        : __updateContent(debouncedValue);
+    console.log("debounced value is ", debouncedValue);
+    if (Object.keys(debouncedValue).length)
+      lessonUpdateDispatch(actionProducer);
+  }, [debouncedValue, lessonUpdateDispatch]);
+  return { dispatch };
+}
