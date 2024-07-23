d106e3967a389a82838bc2747dc709a7129930fe
HazelDaniel <hazeldaniel856@gmail.com>
2024-07-24 00:34:29 +0100

    added ability to pass debounced state update handlers to the input

--- app/components/dashboard-form-input.tsx
+++ app/components/dashboard-form-input.tsx
@@ -7,7 +7,13 @@ import "~/styles/dashboard-form-input.css";
 import { Form, useSearchParams } from "@remix-run/react";
 
 import { ImageReplaceButton } from "./image-replace-button";
-import React, { useRef, useState } from "react";
+import React, {
+  ChangeEvent,
+  FormEvent,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 import { FileUploadFormModal } from "./file-upload-form-modal";
 
 export const InputGraphicWrapper: React.FC<{
@@ -36,7 +42,9 @@ export const GraphicInputForm: React.FC<{
   data: DashboardCustomInputType;
   willDisable: boolean;
   asInput?: boolean;
-}> = ({ data, willDisable, asInput }) => {
+  onChangeHandler?: (e: ChangeEvent<HTMLElement>) => void;
+  onUploadHandler?: (state: [string, string]) => void;
+}> = ({ data, willDisable, asInput, onChangeHandler, onUploadHandler }) => {
   const [action] = data.form.actions;
   const { variant } = data.form;
 
@@ -44,6 +52,10 @@ export const GraphicInputForm: React.FC<{
     data.images[0].url,
     "",
   ]);
+
+  useEffect(() => {
+    if (onUploadHandler) onUploadHandler(imageState);
+  }, [imageState, onUploadHandler]);
 
   return (
     <>
@@ -98,6 +110,7 @@ export const GraphicInputForm: React.FC<{
         inputType={data.inputs[0]?.type}
         setImageState={setImageState}
         originalImage={data.images[0].url}
+        onChangeHandler={onChangeHandler}
       />
     </>
   );
@@ -108,92 +121,123 @@ export const DashboardFormInput: React.FC<{
   defaultData: DefaultFormDataType;
   checkMode?: boolean;
   asInput?: boolean;
-}> = React.memo(({ data, defaultData, checkMode = false, asInput }) => {
-  const [action] = data.form.actions;
-  const { variant } = data.form;
-  const [params] = useSearchParams();
-  const mode = params.get("mode");
-  if (data.images[0])
-    data.images[0].url = defaultData[data.inputs[0].name] as string;
+  onChangeHandler?: (e: ChangeEvent<HTMLElement>) => void;
+  onUploadHandler?: (state: [string, string]) => void;
+}> = React.memo(
+  ({
+    data,
+    defaultData,
+    checkMode = false,
+    asInput,
+    onChangeHandler,
+    onUploadHandler,
+  }) => {
+    const [action] = data.form.actions;
+    const { variant } = data.form;
+    const [params] = useSearchParams();
+    const mode = params.get("mode");
+    if (data.images[0])
+      data.images[0].url = defaultData[data.inputs[0].name] as string;
 
-  const willDisable = checkMode ? (mode === "edit" ? false : true) : false;
+    const willDisable = checkMode ? (mode === "edit" ? false : true) : false;
+    const [e, setE] = useState<ChangeEvent<HTMLElement> | null>(null);
 
-  if (variant === "one-graphic-button") {
+    useEffect(() => {
+      const handler = setTimeout(() => {
+        if (e && onChangeHandler)
+          onChangeHandler(e);
+      }, 1000);
+
+      return () => {
+        clearTimeout(handler);
+      };
+    }, [e]);
+
+    if (variant === "one-graphic-button") {
+      return (
+        <GraphicInputForm
+          data={data}
+          willDisable={willDisable}
+          asInput={asInput}
+          onChangeHandler={onChangeHandler}
+          onUploadHandler={onUploadHandler}
+        />
+      );
+    }
+
     return (
-      <GraphicInputForm
-        data={data}
-        willDisable={willDisable}
-        asInput={asInput}
-      />
+      <Form action={action} className={`input_form ${variant}`}>
+        <div className="input_form_top">
+          {variant === "one-dual-button" || asInput ? null : (
+            <>
+              <p>{data.heading}</p>
+              <button
+                className="input_button_primary"
+                name="intent"
+                value={data.form.intent}
+                disabled={willDisable}
+              >
+                {data.buttons[0]?.text}
+              </button>
+            </>
+          )}
+        </div>
+        <div className="input_form_bottom">
+          {data.inputs.map(({ name, title, type }, idx) => {
+            return (
+              <div className={`input_wrapper ${name}`} key={idx}>
+                <label htmlFor={`${data.namespace}.${name}`}>{title}</label>
+                {type === "textarea" ? (
+                  <textarea
+                    id={`${data.namespace}.${name}`}
+                    name={`${data.namespace}.${name}`}
+                    disabled={willDisable}
+                    maxLength={200}
+                    cols={20}
+                    rows={10}
+                    defaultValue={`${
+                      name === "old_password" || name === "new_password"
+                        ? ""
+                        : defaultData[name] || ""
+                    }`}
+                    onChange={(e) => {
+                      setE(e);
+                    }}
+                  />
+                ) : (
+                  <input
+                    type={type}
+                    id={`${data.namespace}.${name}`}
+                    name={`${data.namespace}.${name}`}
+                    disabled={willDisable}
+                    min={data.inputs[idx].min || 0}
+                    max={data.inputs[idx].max || ""}
+                    defaultValue={`${
+                      name === "old_password" || name === "new_password"
+                        ? ""
+                        : defaultData[name] || ""
+                    }`}
+                    onChange={(e) => {
+                      setE(e);
+                    }}
+                  />
+                )}
+              </div>
+            );
+          })}
+
+          {variant === "one-dual-button" && !asInput ? (
+            <div className="dual_buttons">
+              <button className="input_button_secondary" disabled={willDisable}>
+                {data.buttons[0].text}
+              </button>
+              <button className="input_button_primary" disabled={willDisable}>
+                {data.buttons[1].text}
+              </button>
+            </div>
+          ) : null}
+        </div>
+      </Form>
     );
   }
-
-  return (
-    <Form action={action} className={`input_form ${variant}`}>
-      <div className="input_form_top">
-        {variant === "one-dual-button" || asInput ? null : (
-          <>
-            <p>{data.heading}</p>
-            <button
-              className="input_button_primary"
-              name="intent"
-              value={data.form.intent}
-              disabled={willDisable}
-            >
-              {data.buttons[0]?.text}
-            </button>
-          </>
-        )}
-      </div>
-      <div className="input_form_bottom">
-        {data.inputs.map(({ name, title, type }, idx) => {
-          return (
-            <div className={`input_wrapper ${name}`} key={idx}>
-              <label htmlFor={`${data.namespace}.${name}`}>{title}</label>
-              {type === "textarea" ? (
-                <textarea
-                  id={`${data.namespace}.${name}`}
-                  name={`${data.namespace}.${name}`}
-                  disabled={willDisable}
-                  maxLength={200}
-                  cols={20}
-                  rows={10}
-                  defaultValue={`${
-                    name === "old_password" || name === "new_password"
-                      ? ""
-                      : defaultData[name] || ""
-                  }`}
-                />
-              ) : (
-                <input
-                  type={type}
-                  id={`${data.namespace}.${name}`}
-                  name={`${data.namespace}.${name}`}
-                  disabled={willDisable}
-                  min={data.inputs[idx].min || 0}
-                  max={data.inputs[idx].max || ""}
-                  defaultValue={`${
-                    name === "old_password" || name === "new_password"
-                      ? ""
-                      : defaultData[name] || ""
-                  }`}
-                />
-              )}
-            </div>
-          );
-        })}
-
-        {variant === "one-dual-button" && !asInput ? (
-          <div className="dual_buttons">
-            <button className="input_button_secondary" disabled={willDisable}>
-              {data.buttons[0].text}
-            </button>
-            <button className="input_button_primary" disabled={willDisable}>
-              {data.buttons[1].text}
-            </button>
-          </div>
-        ) : null}
-      </div>
-    </Form>
-  );
-});
+);
