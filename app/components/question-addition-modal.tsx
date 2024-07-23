b61000184497ca42053311c28a3e3b35138553a3
HazelDaniel <hazeldaniel856@gmail.com>
2024-07-23 12:51:07 +0100

    synced the local state with the state in the context, improved the rendering performance a bit (actually, i went more lax on fine-tuning but more on that later) , used default state, renamed svg properties, improved the context state

--- app/components/question-addition-modal.tsx
+++ app/components/question-addition-modal.tsx
@@ -2,10 +2,25 @@ import "~/styles/question-addition-modal.css";
 import "~/styles/question-item-addition-box.css";
 import { QuestionItemAdditionBox } from "./question-item-addition-box";
 import { DashboardFormInput } from "./dashboard-form-input";
-import { DashboardCustomInputType } from "~/types";
-import { useContext } from "react";
+import {
+  DashboardCustomInputType,
+  QuestionModalStateType,
+  QuestionType,
+  StateAnswerType,
+} from "~/types";
+import { useContext, useMemo, useState } from "react";
 import { ModalContext, ModalContextValueType } from "~/contexts/modal.context";
 import { __hideModal } from "~/reducers/modal.reducer";
+import { Form } from "@remix-run/react";
+import { useUpdateQuestionValue } from "~/hooks/use-update-question-value";
+import {
+  QuestionAdditionContextValueType,
+  questionAdditionContext,
+} from "~/contexts/question-addition.context";
+import {
+  __addAnswers,
+  __addQuestion,
+} from "~/reducers/question-addition.reducer";
 
 export const AnswerAdditionFormData: DashboardCustomInputType = {
   heading: "",
@@ -31,10 +46,124 @@ export const AnswerAdditionFormData: DashboardCustomInputType = {
   images: [],
 };
 
+export const AnswerAdditionInput: React.FC<{
+  type: "text" | "checkbox";
+  id: string;
+  modalQuestion: QuestionModalStateType;
+  setModalQuestion: React.Dispatch<
+    React.SetStateAction<QuestionModalStateType>
+  >;
+  questionPosition: number;
+}> = ({ type, modalQuestion, setModalQuestion, id, questionPosition }) => {
+  const { dispatch } = useUpdateQuestionValue(
+    modalQuestion,
+    1000,
+    setModalQuestion
+  );
+  const answerIndex = modalQuestion.answers.findIndex((ans) => ans.id === id);
+  const answerEntry = modalQuestion.answers[answerIndex];
+
+  const [inputPicked, setInputPicked] = useState<boolean>(
+    answerEntry?.correct || false
+  );
+  const [inputText, setInputText] = useState<string>(answerEntry?.text || "");
+  if (type === "text")
+    return (
+      <input
+        type={type}
+        id={`answer${id}`}
+        name="add_answer.answer_text"
+        value={inputText}
+        onChange={(e) => {
+          setInputText(e.target.value);
+          if (e.target.value === answerEntry.text) return;
+          const newState = { ...modalQuestion };
+          newState.answers[answerIndex].text = e.target.value;
+          newState.answers[answerIndex].questionPosition = questionPosition;
+          dispatch(newState);
+        }}
+      />
+    );
+  return (
+    <input
+      type={type}
+      id={`answer${id}_is_correct`}
+      name="add_answer.is_correct"
+      checked={inputPicked}
+      onChange={() => {
+        setInputPicked(!inputPicked);
+        if (!inputPicked === modalQuestion.answers[answerIndex].correct) {
+          return modalQuestion;
+        }
+        const newState = { ...modalQuestion };
+        newState.answers[answerIndex].correct = !inputPicked;
+        newState.answers[answerIndex].questionPosition = questionPosition;
+        dispatch(newState);
+      }}
+    />
+  );
+};
+
+export const AnswerAdditionForm: React.FC<{
+  modalQuestion: QuestionModalStateType;
+  setModalQuestion: React.Dispatch<
+    React.SetStateAction<QuestionModalStateType>
+  >;
+  answerID: string;
+  questionPosition: number;
+}> = ({ modalQuestion, setModalQuestion, answerID, questionPosition }) => {
+  return (
+    <Form method="get" action="/save" className="input_form one-uni-checkbox">
+      <div className="input_form_top"></div>
+      <div className="input_form_bottom">
+        <div className="input_wrapper answer_text">
+          <label htmlFor={`answer${answerID}`}>answer text</label>
+          <AnswerAdditionInput
+            type="text"
+            modalQuestion={modalQuestion}
+            setModalQuestion={setModalQuestion}
+            id={answerID}
+            questionPosition={questionPosition}
+          />
+        </div>
+        <div className="input_wrapper is_correct">
+          <label htmlFor={`answer${answerID}_is_correct`}>is correct</label>
+          <AnswerAdditionInput
+            type="checkbox"
+            modalQuestion={modalQuestion}
+            setModalQuestion={setModalQuestion}
+            id={answerID}
+            questionPosition={questionPosition}
+          />
+        </div>
+      </div>
+    </Form>
+  );
+};
+
 export const QuestionAdditionModal: React.FC = () => {
   const { modalState, modalDispatch } = useContext(
     ModalContext
   ) as ModalContextValueType;
+
+  const { questionAdditionState ,questionAdditionDispatch } = useContext(
+    questionAdditionContext
+  ) as QuestionAdditionContextValueType;
+  const emptyQuestion = useMemo(
+    () =>
+      ({
+        question: { points: 0, position:  questionAdditionState.questions.length},
+        answers: [],
+      } as QuestionModalStateType),
+    []
+  );
+
+  const [modalQuestion, setModalQuestion] =
+    useState<QuestionModalStateType>(emptyQuestion);
+  // console.log(
+  //   "question addition modal rendering with new state",
+  //   modalQuestion
+  // );
 
   return (
     <div
@@ -55,43 +184,69 @@ export const QuestionAdditionModal: React.FC = () => {
             <path
               d="M26 2L2 26M2 2L26 26"
               stroke="var(--icon-fill-here)"
-              stroke-width="4"
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeWidth="4"
+              strokeLinecap="round"
+              strokeLinejoin="round"
             />
           </svg>
         </span>
       </div>
 
       <div className="question_addition_modal_middle">
-        <QuestionItemAdditionBox />
+        <QuestionItemAdditionBox
+          modalQuestion={modalQuestion}
+          setModalQuestion={setModalQuestion}
+        />
         <h2>
           question answers{" "}
-          <button>
+          <button
+            onClick={() => {
+              setModalQuestion((prev) => {
+                const newState = {
+                  ...prev,
+                  answers: [...prev.answers, { id: `${prev.answers.length}` }],
+                };
+                return newState;
+              });
+            }}
+          >
             <svg>
               <use xlinkHref="#add"></use>
             </svg>
           </button>
         </h2>
         <ul className="content_addition_area">
-          <DashboardFormInput
-            data={AnswerAdditionFormData}
-            key={AnswerAdditionFormData.namespace}
-            defaultData={{}}
-            asInput
-          />
-
-          <DashboardFormInput
-            data={AnswerAdditionFormData}
-            key={AnswerAdditionFormData.namespace}
-            defaultData={{}}
-            asInput
-          />
+          {modalQuestion.answers.map((answer) => {
+            return (
+              <AnswerAdditionForm
+                key={answer.id}
+                modalQuestion={modalQuestion}
+                setModalQuestion={setModalQuestion}
+                answerID={answer.id as string}
+                questionPosition={modalQuestion.question.position}
+              />
+            );
+          })}
         </ul>
       </div>
       <div className="question_addition_modal_bottom">
         <button> Cancel </button>
-        <button>Continue</button>
+        <button
+          onClick={() => {
+            if (
+              !modalQuestion.question.question ||
+              modalQuestion.answers.filter((e) => !e.text).length
+            )
+              return;
+            setModalQuestion(emptyQuestion);
+            questionAdditionDispatch(__addQuestion(modalQuestion.question));
+            questionAdditionDispatch(
+              __addAnswers({ answers: modalQuestion.answers })
+            );
+          }}
+        >
+          Continue
+        </button>
       </div>
     </div>
   );
