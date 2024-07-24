6bd61ba97f4e95798d432cf1170a4e7b8974c384
HazelDaniel <hazeldaniel856@gmail.com>
2024-07-24 21:44:35 +0100

    passed the lesson id to the location hash instead of the location pathname to avoid loader hit and thus a full-page re-render, plugged in state handlers

--- app/components/lesson-content-addition-modal.tsx
+++ app/components/lesson-content-addition-modal.tsx
@@ -1,13 +1,64 @@
 import "~/styles/lesson-content-addition-modal.css";
 import { LessonItemAdditionBox } from "./lesson-item-addition-box";
-import React, { useContext } from "react";
+import React, {
+  ChangeEvent,
+  useCallback,
+  useContext,
+  useMemo,
+  useState,
+} from "react";
 import { ModalContext, ModalContextValueType } from "~/contexts/modal.context";
 import { __hideModal } from "~/reducers/modal.reducer";
+import { useLocation, useSearchParams } from "@remix-run/react";
+import { LessonContentAdditionStateType } from "~/types";
 
 export const LessonContentAdditionModal: React.FC = React.memo(() => {
   const { modalState, modalDispatch } = useContext(
     ModalContext
   ) as ModalContextValueType;
+  const location = useLocation();
+  const lessonID = useMemo<number>(
+    () => +(location.hash || ".").split(".")[1],
+    [location.hash]
+  );
+  const [contentAdditionState, setContentAdditionState] =
+    useState<LessonContentAdditionStateType>({});
+
+  const contentAdditionStateHandler = useCallback(
+    (
+      source: ChangeEvent,
+      setFunction: React.Dispatch<
+        React.SetStateAction<LessonContentAdditionStateType>
+      >,
+      keySelector: keyof LessonContentAdditionStateType,
+      isNumeric?: boolean,
+      isCheckable?: boolean,
+      checkedValue?: string
+    ) => {
+      const target: HTMLInputElement = source.target as HTMLInputElement;
+      setFunction((prevState) => {
+        if (!isCheckable && target.value === prevState[keySelector]) {
+          return prevState;
+        } else if (isCheckable && checkedValue === prevState[keySelector]) {
+          return prevState;
+        }
+        if (isCheckable) {
+          return {
+            ...prevState,
+            [keySelector]: isNumeric ? +(checkedValue as string) : checkedValue,
+          };
+        }
+        return {
+          ...prevState,
+          [keySelector]: isNumeric ? +target.value : target.value || target,
+        };
+      });
+    },
+    []
+  );
+
+  if (!lessonID) return null;
+
   return (
     <div
       className={`lesson_content_addition_modal${
@@ -39,7 +90,12 @@ export const LessonContentAdditionModal: React.FC = React.memo(() => {
 
       <div className="lesson_content_addition_modal_middle">
         <ul className="content_addition_area">
-          <LessonItemAdditionBox lessonPositionID={0} itemType="content" />
+          <LessonItemAdditionBox
+            lessonPositionID={+lessonID}
+            itemType="content"
+            stateHandler={contentAdditionStateHandler}
+            additionDispatch={setContentAdditionState}
+          />
         </ul>
       </div>
       <div className="lesson_content_addition_modal_bottom">
