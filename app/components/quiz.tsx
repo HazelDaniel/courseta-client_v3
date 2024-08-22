610362ff242d92886d88254e7d400609d7a531f5
HazelDaniel <hazeldaniel856@gmail.com>
2024-08-22 20:19:19 +0100

    moved the selection logic to the component level (almost cost me my sanity when i realized that a major bug i was having in the db was coming from this), prop-drilled the state update handlers

--- app/components/quiz.tsx
+++ app/components/quiz.tsx
@@ -1,12 +1,70 @@
 import { QuizAnswerType, QuestionType } from "~/types";
 
 import styles from "~/styles/assessment.module.css";
+import { useCallback, useState } from "react";
+import { AssessmentVariantType } from "~/server.types";
+
+export const QuizAnswerInput: React.FC<{
+  value: QuestionType;
+  assessmentID: string;
+  onSelectHandler: (answerQuestion: {
+    questionID: number;
+    answerID: number;
+  }) => void;
+  onUnSelectHandler: (answerQuestion: {
+    questionID: number;
+    answerID: number;
+  }) => void;
+  opt: QuestionType["options"][number];
+  type: "radio" | "check"
+}> = ({ value, assessmentID, onSelectHandler, onUnSelectHandler, opt, type}) => {
+  const [isChecked, setChecked] = useState(false);
+  return (
+    <input
+      type="radio"
+      name={`${value.id}:${value.id}`}
+      id={`${assessmentID}:${value.id}:${opt.id}`}
+      checked={isChecked}
+      // defaultValue={`${assessmentID}-${value.id}-${opt.id}`}
+      onChange={(e: React.FormEvent) => {
+        setChecked((prevChecked) => (type === "radio" ? true : !isChecked));
+        if ((e.currentTarget as HTMLInputElement).checked) {
+          onSelectHandler({
+            questionID: +value.id,
+            answerID: +opt.id,
+          });
+        } else {
+          onUnSelectHandler({
+            questionID: +value.id,
+            answerID: +opt.id,
+          });
+        }
+      }}
+    />
+  );
+};
 
 export const Quiz: React.FC<{
   value: QuestionType;
   key_: number;
-  assessmentID: number;
-}> = ({ value, key_, assessmentID }) => {
+  assessmentID: string;
+  variant: AssessmentVariantType;
+  onSelectHandler: (answerQuestion: {
+    questionID: number;
+    answerID: number;
+  }) => void;
+  onUnSelectHandler: (answerQuestion: {
+    questionID: number;
+    answerID: number;
+  }) => void;
+}> = ({
+  value,
+  key_,
+  assessmentID,
+  variant,
+  onSelectHandler,
+  onUnSelectHandler,
+}) => {
   return (
     <>
       <span>
@@ -17,18 +75,17 @@ export const Quiz: React.FC<{
         {value.options.map((opt: QuizAnswerType, index) => {
           return (
             <div className={styles.option} key={index}>
-              <input
+              <QuizAnswerInput
+                assessmentID={assessmentID}
+                onSelectHandler={onSelectHandler}
+                onUnSelectHandler={onUnSelectHandler}
+                opt={opt}
+                value={value}
+                key={opt.id}
                 type="radio"
-                name={`${assessmentID}-${value.id}`}
-                id={`${assessmentID}-${value.id}-${opt.id}`}
-                // defaultValue={`${assessmentID}-${value.id}-${opt.id}`}
-                onChange={(e: React.FormEvent) => {
-                  if ((e.currentTarget as HTMLInputElement).checked) {
-                    console.log(e.currentTarget);
-                  }
-                }}
               />
-              <label htmlFor={`${assessmentID}-${value.id}-${opt.id}`}>
+
+              <label htmlFor={`${assessmentID}:${value.id}:${opt.id}`}>
                 {opt.text}
               </label>
             </div>
