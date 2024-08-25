a1a72bc6d2e3247ad7d1721d3383ecba37fa30e9
HazelDaniel <hazeldaniel856@gmail.com>
2024-08-25 11:43:17 +0100

    added wrapped components that may access the dom directly in client-only

--- app/components/user-courses.tsx
+++ app/components/user-courses.tsx
@@ -4,35 +4,39 @@ import { CourseFilter } from "../components/course-filter";
 import { NoContent } from "./no-content";
 
 import userCoursesStyles from "~/styles/user-courses.module.css";
+import { CourseViewType } from "~/server.types";
+
 
 export const UserCourses: React.FC<{
   isGeneric: boolean;
-  courses: CourseEntryType[];
+  courses: CourseViewType[];
 }> = ({ isGeneric, courses }) => {
   return (
     <section className={userCoursesStyles.user_courses_styled}>
       <h2>{isGeneric ? "All Courses" : "My Courses"}</h2>
       <div className={userCoursesStyles.courses_bottom}>
-        <ul className={userCoursesStyles.courses_list_container}>
-          {courses.length ? (
-            courses.map((entry) => {
-              return (
-                <SmallCourseCard
-                  entry={entry}
-                  withCTA={isGeneric}
-                  variant="others"
-                  key={entry.id}
-                />
-              );
-            })
-          ) : (
-            <NoContent text="No courses to show" variant="courses" />
-          )}
-        </ul>
-
-        {/* <div className="courses_pagination_area">
-          <Pagination itemsPerPage={5} key={1}/>
-        </div> */}
+        {/* <ClientOnly>
+          {() => {
+            return ( */}
+              <ul className={userCoursesStyles.courses_list_container}>
+                {courses.length ? (
+                  courses.map((entry) => {
+                    return (
+                      <SmallCourseCard
+                        entry={entry}
+                        withCTA={isGeneric}
+                        variant="others"
+                        key={entry.courseID}
+                      />
+                    );
+                  })
+                ) : (
+                  <NoContent text="No courses to show" variant="courses" />
+                )}
+              </ul>
+            {/* );
+          }}
+        </ClientOnly> */}
       </div>
       <div
         className={`${userCoursesStyles.courses_top}${
