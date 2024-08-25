11a4ca9cff90e345b5d95092193b4d5a4eecfbc2
HazelDaniel <hazeldaniel856@gmail.com>
2024-08-25 11:49:51 +0100

    added a loader and action, split components

--- app/routes/_app.courses_.$course_id.reviews/route.tsx
+++ app/routes/_app.courses_.$course_id.reviews/route.tsx
@@ -1,128 +1,245 @@
-import { LoaderFunction } from "@remix-run/node";
-import { Form, json, useLoaderData, useOutletContext } from "@remix-run/react";
+import { ActionFunction, LoaderFunction, redirect } from "@remix-run/node";
+import {
+  json,
+  useFetcher,
+  useLoaderData,
+  useOutletContext,
+} from "@remix-run/react";
+import axios, { AxiosError, AxiosResponse } from "axios";
+import { useState } from "react";
+import { jsonWithError, jsonWithSuccess } from "remix-toast";
+import { toast } from "sonner";
+import { NoContent } from "~/components/no-content";
 import { RatingForm } from "~/components/rating-form";
+import { v3Config } from "~/config/base";
 import { courseReviews } from "~/data/course-reviews";
+import { serializeStudentReviewForm } from "~/serializers/review.serializer";
+import { ServerPayloadType } from "~/server.types";
 import "~/styles/course-reviews.css";
-import { CourseDetailType, CourseReviewType } from "~/types";
+import {
+  ActionResponseType,
+  CourseDetailType,
+  CourseReviewType,
+  LoaderResponseType,
+  SessionUserType,
+  StudentReviewActionType,
+} from "~/types";
 
-export const loader: LoaderFunction = () => {
-  return json({ reviews: courseReviews });
+export const loader: LoaderFunction = async ({ request, params }) => {
+  try {
+    const { course_id: courseID } = params;
+    const cookieHeader = request.headers.get("Cookie");
+    const reviewsRequest: AxiosResponse<
+      ServerPayloadType<CourseReviewType[]>,
+      any
+    > = await axios.get(`${v3Config.apiUrl}/courses/${courseID}/reviews`, {
+      headers: {
+        Cookie: cookieHeader,
+      },
+    });
+
+    if (reviewsRequest.status !== 200) {
+      if (reviewsRequest.status - 500 >= 0)
+        throw json(
+          {
+            data: null,
+            error: "an error occurred while fetching course reviews.",
+          } as LoaderResponseType<null>,
+          500
+        );
+      throw redirect("/auth?type=sign_in");
+    }
+
+    return json({
+      reviews: reviewsRequest.data.payload,
+      user: reviewsRequest.data.user,
+    });
+  } catch (err) {
+    if (err instanceof Response) {
+      throw err;
+    }
+    throw json(
+      { error: (err as Error)?.message || "An unexpected error occurred" },
+      { status: 500, statusText: "Internal Server Error" }
+    );
+  }
 };
 
-export const CourseReviews: React.FC = () => {
-  const outletCourse = useOutletContext() as CourseDetailType;
-  const { reviews } = useLoaderData<typeof loader>() as {
-    reviews: CourseReviewType[];
+const CourseReviewArea: React.FC = () => {
+  const fetcher = useFetcher({ key: "student-review" });
+  const { user } = useLoaderData<typeof loader>() as {
+    user: SessionUserType | undefined;
   };
-  return (
-    <div className="course_reviews_area">
-      <div className="course_reviews_top">
-        <ul>
-          {reviews.map((reviewEntry, idx) => {
-            return (
-              <li className="review_entry">
-                <img
-                  src={reviewEntry.studentAvatarURL}
-                  alt="the image url of a student making a review on a course"
-                />
-                <div className="review_entry_right">
-                  <div className="entry_message_top">
-                    <h2>{reviewEntry.studentEmail}</h2>
-                    <span>
-                      {new Date(reviewEntry.dateCreated).toDateString()}
-                    </span>
-                  </div>
-                  <div className="entry_rating_area">
-                    <div className="rating_container">
-                      <RatingForm
-                        variant="view"
-                        value={reviewEntry.rating}
-                        namespace={reviewEntry.studentEmail}
-                      />
-                    </div>
-                  </div>
-                  <p className="entry_message_body">{reviewEntry.reviewText}</p>
-                </div>
-              </li>
-            );
-          })}
+  const [rating, setRating] = useState(0);
 
-          {/* <li className="review_entry">
-            <img
-              src="/illustrations/person-admin.png"
-              alt="the image url of a student making a review on a course"
-            />
-            <div className="review_entry_right">
-              <div className="entry_message_top">
-                <h2>peterstokes20@gmail.com</h2>
-                <span>10 mins ago</span>
-              </div>
-              <div className="entry_rating_area">
-                <div className="rating_container">
-                  <span>*</span>
-                  <span>*</span>
-                  <span>*</span>
-                  <span>*</span>
-                  <span>*</span>
-                </div>
-              </div>
-              <p className="entry_message_body">
-                i just don't like this course. it's full of typos. can i get my
-                refund back please?
-              </p>
-            </div>
-          </li>
-          <li className="review_entry">
-            <img
-              src="/illustrations/person-admin.png"
-              alt="the image url of a student making a review on a course"
-            />
-            <div className="review_entry_right">
-              <div className="entry_message_top">
-                <h2>peterstokes20@gmail.com</h2>
-                <span>10 mins ago</span>
-              </div>
-              <div className="entry_rating_area">
-                <div className="rating_container">
-                  <span>*</span>
-                  <span>*</span>
-                  <span>*</span>
-                  <span>*</span>
-                  <span>*</span>
-                </div>
-              </div>
-              <p className="entry_message_body">
-                i just don't like this course. it's full of typos. can i get my
-                refund back please?
-              </p>
-            </div>
-          </li> */}
-        </ul>
-      </div>
-      <RatingForm variant="edit" />
+  return (
+    <>
+      <RatingForm variant="edit" onChangeHandler={(x) => setRating(x)} />
       <div className="course_review_create_area">
         <img
           src="/illustrations/avatar1.jpg"
           alt="image of a student creating a review on a course"
+          loading="lazy"
         />
-        <Form action="./" method="post">
+
+        <fetcher.Form
+          action="./"
+          method="post"
+          onSubmit={async (e) => {
+            e.preventDefault();
+            const data = Object.fromEntries(
+              new FormData(e.currentTarget as HTMLFormElement)
+            );
+            const reviewPayload = serializeStudentReviewForm(
+              data as { review_text: string },
+              rating,
+              user?.id || ""
+            );
+            fetcher.submit(reviewPayload as any, {
+              action: "./",
+              method: "post",
+              encType: "application/json",
+            });
+          }}
+        >
           <textarea
-            name="review_text"
+            name={"review_text"}
             id=""
             cols={30}
             rows={10}
             maxLength={250}
             placeholder="write your review..."
           ></textarea>
-          <button type="submit">
+          <button
+            type="submit"
+            onClick={(e) => {
+              if (user && user.role === "creator") {
+                e.preventDefault();
+                toast.error("you are not allowed to review a course as a creator!")
+                return;
+              }
+              //  some pop-up message to tell them to enroll first if they have not already enrolled
+              //  OR some pop-up message to tell them to authenticate first if they have not already authenticated
+              console.log("submitting a review i see");
+            }}
+          >
             <svg>
               <use xlinkHref="#send"></use>
             </svg>
           </button>
-        </Form>
+        </fetcher.Form>
       </div>
+    </>
+  );
+};
+
+export const CourseReviews: React.FC = () => {
+  const { reviews } = useLoaderData<typeof loader>() as {
+    reviews: CourseReviewType[];
+  };
+
+  return (
+    <div className="course_reviews_area">
+      <div className="course_reviews_top">
+        <ul>
+          {reviews.length ? (
+            reviews.map((reviewEntry, idx) => {
+              return (
+                <li className="review_entry" key={idx}>
+                  <img
+                    src={reviewEntry.avatar}
+                    alt="the image url of a student making a review on a course"
+                    loading="lazy"
+                  />
+                  <div className="review_entry_right">
+                    <div className="entry_message_top">
+                      <h2>{reviewEntry.studentEmail}</h2>
+                      <span>
+                        {new Date(reviewEntry.dateCreated).toDateString()}
+                      </span>
+                    </div>
+                    <div className="entry_rating_area">
+                      <div className="rating_container">
+                        <RatingForm
+                          variant="view"
+                          value={reviewEntry.rating}
+                          namespace={reviewEntry.studentEmail}
+                        />
+                      </div>
+                    </div>
+                    <p className="entry_message_body">
+                      {reviewEntry.reviewText}
+                    </p>
+                  </div>
+                </li>
+              );
+            })
+          ) : (
+            <NoContent
+              text="no reviews for this course"
+              variant="course_outline"
+            />
+          )}
+        </ul>
+      </div>
+      <CourseReviewArea />
     </div>
   );
 };
 
 export default CourseReviews;
+
+export const action: ActionFunction = async ({ request, params }) => {
+  try {
+    const reqJson = (await request.json()) as StudentReviewActionType;
+    const { course_id: courseID } = params;
+    const cookieHeader = request.headers.get("Cookie");
+    let actionRequest: AxiosResponse<ServerPayloadType<void>, any>;
+    let requestURL: string;
+    switch (reqJson.intent) {
+      case "REVIEW_COURSE": {
+        requestURL = `${v3Config.apiUrl}/courses/${courseID}/reviews`;
+        actionRequest = await axios.post(requestURL, reqJson.payload, {
+          headers: {
+            Cookie: cookieHeader,
+          },
+        });
+        if (actionRequest.status !== 201) break;
+        return jsonWithSuccess(null, "review posted!")
+      }
+    }
+
+    if (actionRequest.status - 500 >= 0) {
+      throw json({ error: "something went wrong" }, 500);
+    } else {
+      return jsonWithError(
+        null,
+        `couldn't proceed with action. REASON: ${actionRequest.data.message}`
+      );
+    }
+    // return json({
+    //   data: null,
+    //   error: `couldn't proceed with action. REASON: ${actionRequest.data.message}`,
+    // } as ActionResponseType<null>);
+  } catch (err) {
+    if (err instanceof AxiosError) {
+      console.error(err.response?.data.message);
+      return jsonWithError(null, err.response?.data.message || "couldn't proceeed with action!")
+    }
+    if (err instanceof Response) {
+      if (err.status >= 300 && err.status < 400) 
+      throw err;
+    }
+    throw json(
+      {
+        error:
+          err instanceof Error ? err.message : "could not proceed with action",
+      },
+      500
+    );
+  }
+};
+
+export const ErrorBoundary: React.FC = () => {
+  return <h2>na here e take happen. no dey whine boss</h2>;
+};
