f32a4d9b58e2fbe6a4227e2270dd7646f26d4a3d
HazelDaniel <hazeldaniel856@gmail.com>
2024-08-25 12:00:15 +0100

    added the logic to fetch exam info with questions in the loader, handled submission logic in the action

--- app/routes/_app.courses_.$course_id_.exams_.$exam_id/route.tsx
+++ app/routes/_app.courses_.$course_id_.exams_.$exam_id/route.tsx
@@ -1,5 +1,10 @@
 import styles from "~/styles/assessment.module.css";
-import { LoaderFunction, json } from "@remix-run/node";
+import {
+  ActionFunction,
+  LoaderFunction,
+  json,
+  redirect,
+} from "@remix-run/node";
 import {
   isRouteErrorResponse,
   useLoaderData,
@@ -8,32 +13,89 @@ import {
 import { AssessmentBody } from "~/components/course-details";
 import { NotFound } from "~/components/not-found";
 import { courseDataDetailed } from "~/data/course-list";
-import { CourseDetailType, CourseExamType } from "~/types";
+import {
+  ActionResponseType,
+  AssessmentSubmissionActionType,
+  AssessmentSubmissionPayloadType,
+  CourseDetailType,
+  CourseExamType,
+  CourseExamType2,
+  LoaderResponseType,
+  QuestionType,
+} from "~/types";
+import axios, { AxiosError, AxiosResponse } from "axios";
+import { ServerPayloadType } from "~/server.types";
+import { v3Config } from "~/config/base";
+import { redirectWithToast } from "remix-toast";
 
+export const loader: LoaderFunction = async ({ params, request }) => {
+  const { exam_id: assessmentID } = params;
+  const cookieHeader = request.headers.get("Cookie");
+  try {
+    const examPromise: Promise<
+      AxiosResponse<ServerPayloadType<CourseExamType2 | null>, any>
+    > = axios.get(`${v3Config.apiUrl}/exams/${assessmentID}`, {
+      headers: {
+        Cookie: cookieHeader,
+      },
+    });
 
-export const loader: LoaderFunction = ({ params }) => {
-  const examID = params["exam_id"];
-  const courseID = params["course_id"];
+    const questionsPromise: Promise<
+      AxiosResponse<ServerPayloadType<QuestionType[]>, any>
+    > = axios.get(`${v3Config.apiUrl}/assessments/${assessmentID}/questions`, {
+      headers: {
+        Cookie: cookieHeader,
+      },
+    });
 
-  const course = courseDataDetailed.filter(
-    (course) => course.id === +(courseID as string)
-  )[0];
+    const allPromises = await Promise.all([examPromise, questionsPromise]);
+    const [examInfo, questionsInfo] = allPromises;
+    if (examInfo.status !== 200) {
+      if (examInfo.status - 500 >= 0)
+        throw json(
+          {
+            data: null,
+            error: "an error occurred while fetching exam data.",
+          } as LoaderResponseType<null>,
+          500
+        );
+      throw redirect("/auth?type=sign_in");
+    }
+    
+    if (!examInfo.data.payload) {
+      throw json({ error: "exam not found!" }, { status: 404 });
+    }
+    if (!examInfo.data.user) {
+      throw redirect("/auth?type=sign_in");
+    }
+    if (!examInfo.data.payload?.ready) {
+      return redirectWithToast("/", {type: "info", description: "you will be notified when it's ready", message: "this exam hasn't started yet!"})
+    }
 
-  if (!course) {
-    throw json({ error: "course not found!" }, { status: 404 });
+    return {
+      exam: examInfo.data.payload,
+      questions: questionsInfo.data.payload,
+      user: examInfo.data.user,
+    };
+  } catch (err) {
+    if (err instanceof Response) {
+      throw err;
+    }
+    throw json(
+      { error: (err as Error)?.message || "An unexpected error occurred" },
+      { status: 500, statusText: "Internal Server Error" }
+    );
   }
-
-  return json({ course, examID: +(examID as string) });
 };
 
 export const ExamPage: React.FC = () => {
-  const res = useLoaderData<typeof loader>() as {
-    examID: string;
-    course: CourseDetailType;
-  };
-  return <div className={`${styles.course_accessment_area} ${styles.course_assessment_area_styled}`}>
-    <AssessmentBody assessment={res.course?.exam as CourseExamType} />;
-  </div>;
+  return (
+    <div
+      className={`${styles.course_accessment_area} ${styles.course_assessment_area_styled}`}
+    >
+      <AssessmentBody variant="exam" />;
+    </div>
+  );
 };
 
 export const ErrorBoundary: React.FC = () => {
@@ -51,3 +113,55 @@ export const ErrorBoundary: React.FC = () => {
 };
 
 export default ExamPage;
+
+export const action: ActionFunction = async ({ params, request }) => {
+  let actionRequest: AxiosResponse<ServerPayloadType<void>, any> | null = null;
+  try {
+    const { exam_id: assessmentID } = params;
+    const reqJson = (await request.json()) as AssessmentSubmissionActionType;
+    const cookieHeader = request.headers.get("Cookie");
+    let requestURL: string;
+    switch (reqJson.intent) {
+      case "SUBMIT": {
+        requestURL = `${v3Config.apiUrl}/assessments/${assessmentID}/submit`;
+        actionRequest = await axios.post(
+          requestURL,
+          {
+            ...reqJson.payload,
+            submissionTime: new Date().toISOString(),
+          } as AssessmentSubmissionPayloadType,
+          {
+            headers: {
+              Cookie: cookieHeader,
+            },
+          }
+        );
+        if (actionRequest?.status !== 200) break;
+        return redirectWithToast(`/`, {
+          message: "exam submission success!",
+          type: "info",
+          description:
+            "you can now check your results in the 'assessment results' tab ",
+        });
+      }
+    }
+  } catch (err) {
+    if (err instanceof AxiosError) {
+      return redirectWithToast(`/`, {
+        message: `couldn't proceed with action. REASON: ${
+          err.response?.data?.message || "unknown"
+        }`,
+        type: "error",
+      });
+    }
+    if (err instanceof Response && err.status >= 300 && err.status < 400)
+      throw err;
+    throw json(
+      {
+        error:
+          err instanceof Error ? err.message : "could not proceed with action",
+      },
+      500
+    );
+  }
+};
