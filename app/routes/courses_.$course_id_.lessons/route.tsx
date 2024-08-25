b03bf4646419ace1e6449fb5328e8bae0c9a504a
HazelDaniel <hazeldaniel856@gmail.com>
2024-08-25 11:20:44 +0100

    removed the dummy data from the loader

--- app/routes/courses_.$course_id_.lessons/route.tsx
+++ app/routes/courses_.$course_id_.lessons/route.tsx
@@ -1,16 +1,13 @@
-import { LoaderFunction, json } from "@remix-run/node";
+import { LoaderFunction, json, redirect } from "@remix-run/node";
 import App from "../_app/route";
 import { courseDataDetailed } from "~/data/course-list";
+import axios, { AxiosResponse } from "axios";
+import { CourseDetailViewType, ServerPayloadType } from "~/server.types";
+import { v3Config } from "~/config/base";
+import { CourseLessonType2, LoaderResponseType } from "~/types";
 
-export const loader: LoaderFunction = ({ params }) => {
-  const courseID = params["course_id"];
-
-  const course = courseDataDetailed.filter(
-    (course) => course.id === +(courseID as string)
-  )[0];
-
-
-  return json({ course });
+export const loader: LoaderFunction = () => {
+  return {};
 };
 
 export const LessonPage: React.FC = () => {
