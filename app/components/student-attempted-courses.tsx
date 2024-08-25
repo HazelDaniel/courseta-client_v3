af854e5b87c2bd68f39be798a077f06816c07d28
HazelDaniel <hazeldaniel856@gmail.com>
2024-08-25 11:42:17 +0100

    added fallback components for empty entries

--- app/components/student-attempted-courses.tsx
+++ app/components/student-attempted-courses.tsx
@@ -2,25 +2,34 @@ import { CourseEntryType } from "~/types";
 import { WideCourseCard } from "./wide-course-card";
 
 import "~/styles/student-attempted-courses.css";
+import { NoContent } from "./no-content";
+import { useAsyncValue } from "@remix-run/react";
+import { ServerPayloadType, StudentCourseViewType } from "~/server.types";
+import { AxiosResponse } from "axios";
 
-export const StudentAttemptedCourses: React.FC<{
-  courses: CourseEntryType[];
-}> = ({ courses }) => {
+export const StudentAttemptedCourses: React.FC<{courses: StudentCourseViewType[]}> = ({courses}) => {
   return (
     <div className="student_profile_middle_area">
       <div className="profile_bottom_title_div">
         <h2>Recently Attempted Courses</h2>
         <button>view all courses</button>
-      </div>{" "}
+      </div>
       <div className="attempted_courses_area">
         <ul>
-          {courses.map((entry, idx) => {
-            return (
-              <li key={idx}>
-                <WideCourseCard entry={entry} />
-              </li>
-            );
-          })}
+          {courses.length ? (
+            courses.map((entry, idx) => {
+              return (
+                <li key={idx}>
+                  <WideCourseCard entry={entry} />
+                </li>
+              );
+            })
+          ) : (
+            <NoContent
+              text="No unfinished courses to display"
+              variant="course_outline"
+            />
+          )}
         </ul>
       </div>
     </div>
