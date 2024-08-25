7e039260ef013bd99f40c9e7426f4ea937deef73
HazelDaniel <hazeldaniel856@gmail.com>
2024-08-25 11:57:08 +0100

    updated the loader to fetch the correct data

--- app/routes/_app.courses_.$course_id/route.tsx
+++ app/routes/_app.courses_.$course_id/route.tsx
@@ -7,32 +7,97 @@ import {
 } from "@remix-run/react";
 import { NotFound } from "~/components/not-found";
 import { courseDataDetailed } from "~/data/course-list";
-import type { CourseDetailType } from "~/types";
+import type {
+  CourseCreatorViewType,
+  CourseDetailType,
+  CourseLessonType2,
+  LoaderResponseType,
+} from "~/types";
 import { CourseBanner } from "./course-banner";
 import { CourseInfo } from "./course-info";
 import styles from "~/styles/course.module.css";
-import { LinksFunction, LoaderFunction } from "@remix-run/node";
+import { LinksFunction, LoaderFunction, redirect } from "@remix-run/node";
+import axios, { AxiosResponse } from "axios";
+import { CourseDetailViewType, ServerPayloadType } from "~/server.types";
+import { v3Config } from "~/config/base";
 
 // export const links: LinksFunction = () => {
 //   return [{ rel: "stylesheet", href: styles }];
 // };
 
-export const loader: LoaderFunction = ({ params }) => {
-  const courseID = params["course_id"];
+export const loader: LoaderFunction = async ({ params, request }) => {
+  const { course_id: courseID } = params;
+  const cookieHeader = request.headers.get("Cookie");
 
-  const course = courseDataDetailed.filter(
-    (course) => course.id === +(courseID as string)
-  )[0];
+  try {
+    // console.log("toast on the course id route is ", toast);
+    // return json({toast}, {headers});
+    const coursePromise: Promise<
+      AxiosResponse<ServerPayloadType<CourseDetailViewType>, any>
+    > = axios.get(`${v3Config.apiUrl}/courses/${courseID}/`, {
+      headers: {
+        Cookie: cookieHeader,
+      },
+    });
 
-  if (!course) {
-    throw json({ error: "course not found!" }, { status: 404 });
+    const courseCreatorPromise: Promise<
+      AxiosResponse<ServerPayloadType<CourseCreatorViewType[]>, any>
+    > = axios.get(`${v3Config.apiUrl}/courses/${courseID}/creator/summary`, {
+      headers: {
+        Cookie: cookieHeader,
+      },
+    });
+
+    const courseLessonsPromise: Promise<
+      AxiosResponse<ServerPayloadType<CourseLessonType2[]>, any>
+    > = axios.get(`${v3Config.apiUrl}/courses/${courseID}/lessons`, {
+      headers: {
+        Cookie: cookieHeader,
+      },
+    });
+
+    const allPromises = await Promise.all([
+      coursePromise,
+      courseLessonsPromise,
+      courseCreatorPromise,
+    ]);
+    let [courseInfo, lessons, creatorInfo] = allPromises;
+    if (courseInfo.status !== 200) {
+      if (courseInfo.status - 500 >= 0)
+        throw json(
+          {
+            data: null,
+            error: "an error occurred while fetching course data.",
+          } as LoaderResponseType<null>,
+          500
+        );
+      throw redirect("/auth?type=sign_in");
+    }
+
+    if (!courseInfo.data.payload) {
+      throw json({ error: "course not found!" }, { status: 404 });
+    }
+
+    return json({
+      course: courseInfo.data.payload,
+      creator: creatorInfo.data.payload,
+      lessons: lessons.data.payload,
+      user: courseInfo.data.user,
+    });
+  } catch (err) {
+    if (err instanceof Response) {
+      throw err;
+    }
+    throw json(
+      { error: (err as Error)?.message || "An unexpected error occurred" },
+      { status: 500, statusText: "Internal Server Error" }
+    );
   }
-
-  return json({ course });
 };
 
 const Course: React.FC = () => {
-  const { course } = useLoaderData<typeof loader>();
+  const { course }: { course: CourseDetailViewType } =
+    useLoaderData<typeof loader>();
 
   return (
     <div className={styles.course_styled}>
