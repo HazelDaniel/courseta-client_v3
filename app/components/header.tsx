19ebf301303ee4215dc7c721886a2742f73d1218
HazelDaniel <hazeldaniel856@gmail.com>
2024-08-30 17:03:38 +0100

    added a header component for the home route

---
+++ app/components/header.tsx
@@ -0,0 +1,53 @@
+import headerStyle from "~/styles/landing-header.module.css";
+import { Link, useNavigate } from "@remix-run/react";
+
+export const Header: React.FC = () => {
+  const navigate = useNavigate();
+  return (
+    <header className={headerStyle["header"]}>
+      <div className={headerStyle["logo_parent"]}>
+        <img
+          src="/logo.svg"
+          alt="the logo of the ed-tech platform, courseta. courseta ed-tech logo"
+        />
+      </div>
+      <div className={headerStyle["header_child"]}>
+        <nav className={headerStyle["header_links_area"]}>
+          <ul>
+            <li>
+              <Link to="">home</Link>
+            </li>
+            <li>
+              <Link to="">about us</Link>
+            </li>
+            <li>
+              <Link to="">vision</Link>
+            </li>
+            <li>
+              <Link to="/home/#faq">Faq</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <div className={headerStyle["header_cta_area"]}>
+          <button
+            className={
+              headerStyle["btn_secondary"] + " " + headerStyle["header_btn"]
+            }
+            onClick={() => navigate("/auth?type=sign_up&role=creator")}
+          >
+            be a creator
+          </button>
+          <button
+            className={
+              headerStyle["btn_primary"] + " " + headerStyle["header_btn"]
+            }
+            onClick={() => navigate("/auth?type=sign_in&role=student")}
+          >
+            be a student
+          </button>
+        </div>
+      </div>
+    </header>
+  );
+};
