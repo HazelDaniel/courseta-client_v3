10cfd3f3674b012fceeaca244d694083a77303dc
HazelDaniel <hazeldaniel856@gmail.com>
2024-08-31 06:32:22 +0100

    fixed the api endpoint resolution for prod

--- app/config/base.ts
+++ app/config/base.ts
@@ -1,12 +1,12 @@
 export const BASE_URL: string =
-  import.meta.env.VITE_ENV === "dev"
-    ? import.meta.env.VITE_BASE_URL_DEV
-    : import.meta.env.VITE_BASE_URL_PROD;
+import.meta.env.VITE_ENV === "prod"
+? import.meta.env.VITE_BASE_URL_PROD
+    : import.meta.env.VITE_BASE_URL_DEV;
 
 export const IMAGE_URL: string =
-  import.meta.env.VITE_ENV === "dev"
-    ? import.meta.env.VITE_IMAGE_URL_DEV
-    : import.meta.env.VITE_IMAGE_URL_PROD;
+  import.meta.env.VITE_ENV === "prod"
+  ? import.meta.env.VITE_IMAGE_URL_PROD
+    : import.meta.env.VITE_IMAGE_URL_DEV;
 
 export const FAKE_REQUEST_DELAY = 2000;
 
