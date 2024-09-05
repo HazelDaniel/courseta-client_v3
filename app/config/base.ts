c918a93c4aa1cbef89769782122364938d12e718
HazelDaniel <hazeldaniel856@gmail.com>
2024-09-05 08:24:47 +0100

    adapting the codebase to server version changes

--- app/config/base.ts
+++ app/config/base.ts
@@ -1,16 +1,16 @@
 export const BASE_URL: string =
-import.meta.env.VITE_ENV === "prod"
-? import.meta.env.VITE_BASE_URL_PROD
+  import.meta.env.VITE_ENV === "prod"
+    ? import.meta.env.VITE_BASE_URL_PROD
     : import.meta.env.VITE_BASE_URL_DEV;
 
 export const IMAGE_URL: string =
   import.meta.env.VITE_ENV === "prod"
-  ? import.meta.env.VITE_IMAGE_URL_PROD
+    ? import.meta.env.VITE_IMAGE_URL_PROD
     : import.meta.env.VITE_IMAGE_URL_DEV;
 
 export const FAKE_REQUEST_DELAY = 2000;
 
 export const v3Config = {
-  apiUrl: `${BASE_URL}/api/v1`,
-  imageAPIUrl: `${IMAGE_URL}/api/v1/images`
-}
+  apiUrl: `${BASE_URL}/api/v2`,
+  imageAPIUrl: `${IMAGE_URL}/api/v2/images`,
+};
